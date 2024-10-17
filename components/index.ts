8a57d65c0f6ef71932cf227aac891289c33f75cf
GastonSKL <gastonlskamlec@gmail.com>
2024-10-17 03:30:03 -0300

    [add][17/10/2024][carhub] add show more cars function

--- components/index.ts
+++ components/index.ts
@@ -7,6 +7,7 @@ import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard";
 import CardDetails from "./CardDetails";
+import ShowMore from "./ShowMore";
 export {
     Hero,
     CustomButton,
@@ -16,5 +17,6 @@ export {
     SearchBar,
     SearchManufacturer,
     CarCard,
-    CardDetails
+    CardDetails,
+    ShowMore
 }
